ddb50c7fd88d5b14dcbeb2f804332f60ae4bc349
Tomi T. Salo <ttsalo@iki.fi>
2025-11-03 17:49:51 +0200

    Nice labels for coordinate grid

--- obs-ui/src/obs.jsx
+++ obs-ui/src/obs.jsx
@@ -8,7 +8,7 @@ function azToPx(az, stageSize) {
 }
 
 function altToPx(alt, stageSize) {
-    return (alt - stageSize.get("minAlt")) * stageSize.get("zoom");
+    return (stageSize.get("maxAlt") - alt) * stageSize.get("zoom");
 }
 
 const ObsStage = () => {
@@ -23,19 +23,58 @@ const ObsStage = () => {
 	// Draw the coordinate grid
 	const l = coordsLayer.current;
 	l.destroyChildren();
+	// Azimuth lines (vertical)
 	for (let i = 0; i < 360; i = i + 10) {
 	    const line = new Konva.Line(
 		{points: [azToPx(i, stageSize), altToPx(-90, stageSize),
 			  azToPx(i, stageSize), altToPx(90, stageSize)],
 		 stroke: "#808080", strokeWidth: 1});
 	    l.add(line);
+	    const label = new Konva.Label({
+		x: azToPx(i, stageSize), 
+		y: altToPx(stageSize.get("maxAlt"), stageSize)});
+	    label.add(
+		new Konva.Tag({
+		    pointerDirection: 'left',
+		    pointerWidth: 6,
+		    pointerHeight: 6,
+		    lineJoin: 'round',
+		    fill: 'white',
+		    stroke: "#808080",
+		    strokeWidth: 1
+		})
+	    );
+	    label.add(new Konva.Text({text: `${i}°`, padding: 2,
+				       fill: "black"}));
+	    label.setAttrs({y: label.getAttr('y') + label.height()});
+	    l.add(label);
 	}
+	
+	// Altitude lines (horizontal)
 	for (let i = -90; i <= 90; i = i + 10) {
 	    const line = new Konva.Line(
 		{points: [azToPx(0, stageSize), altToPx(i, stageSize),
 			  azToPx(360, stageSize), altToPx(i, stageSize)],
 		 stroke: "#808080", strokeWidth: 1});
 	    l.add(line);
+	    const label = new Konva.Label({
+		x: azToPx(stageSize.get("minAz"), stageSize),
+		y: altToPx(i, stageSize)});
+	    label.add(
+		new Konva.Tag({
+		    pointerDirection: 'up',
+		    pointerWidth: 6,
+		    pointerHeight: 6,
+		    lineJoin: 'round',
+		    fill: 'white',
+		    stroke: "#808080",
+		    strokeWidth: 1
+		})
+	    );
+	    label.add(new Konva.Text({text: `${i}°`, padding: 2,
+				       fill: "black"}));
+	    label.setAttrs({x: label.getAttr('x') + label.width()});
+	    l.add(label);
 	}
     }
     
